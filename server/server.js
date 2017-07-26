305b0024a99d61a8351c68b3dde91b97b9a207bd
Lyubomir Vezev <l.vezev@gmail.com>
2017-07-26 19:55:47 +0300

    Setup express

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
+require('./config/config');
+
 const express = require('express');
 
 var app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
